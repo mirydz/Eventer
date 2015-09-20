da478366660ed48857f17fa1f27b72468893274a
Michal Rydz <mirydz@gmail.com>
2015-09-20 18:14:16 +0200

    improvements & fixes

--- js/app.js
+++ js/app.js
@@ -8,18 +8,41 @@
 		this.description = params.description || "";
 		this.time = params.time;
 		this.reminderTime = params.reminderTime;
+		this.isReminderSent = false;
 	}
 	
-	MyEvent.prototype.formattedTime = function() {
-		return this.time.toLocaleString();
+	MyEvent.prototype.formatTime = function(date) {
+		var monthNames = [
+			"January", "February", "March",
+			"April", "May", "June", "July",
+			"August", "September", "October",
+			"November", "December"
+			];
+		
+		var day = date.getDate();
+		var monthIndex = date.getMonth();
+		var year = date.getFullYear();
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
+				
+		return day +" "+ monthNames[monthIndex] + " "
+				+ year + " " + hours + ":" + minutes;
 	};
 	
+	MyEvent.prototype.formattedTime = function() {
+		
+		//return this.time.toLocaleString();
+		return this.formatTime(this.time);
+	}
+	
 	MyEvent.prototype.formattedReminderTime = function() {
-		return this.reminderTime.toLocaleString();
+		return this.formatTime(this.reminderTime);
 	};
 	
 	var events = (function() {
-		var eventsList =  [];
 		var store = new Dexie("events-store");
 		store.version(1).stores({event: '++id', });
 		store.event.mapToClass(MyEvent);
@@ -33,35 +56,11 @@
 		}
 		
 		function add(newEvent) {
-			//eventsList.push(newEvent);
-			var ev = {
-				title: 			newEvent.title,
-				description: 	newEvent.description,
-				time: 			newEvent.time,
-				reminderTime: 	newEvent.reminderTime,
-			};
-			ev.reminderTime = newEvent.reminderTime;
-			store.event.put(ev);
+			store.event.add(newEvent);
 		}
 		
 		function update(updatedEvent) {
-			store.event.put({
-				title: updatedEvent.title,
-				description: updatedEvent.description,
-				time: updatedEvent.time,
-				reminderTime: updatedEvent.reminderTime,
-				id: updatedEvent.id
-			});
-		}
-		
-		function getById(id) {
-			var eventWeLookFor = null;
-			eventsList.forEach(function(element) {
-				if (element.id == id) {
-					eventWeLookFor = element;
-				}
-			}, this);
-			return eventWeLookFor;
+			store.event.put(updatedEvent);
 		}
 		
 		function removeById(eventId) {
@@ -72,17 +71,23 @@
 			var initialData = [	
 				{
 					title: "Tom's party",
-					time: new Date(2015, 10, 30),
-					description: "Remember to bering beer"
+					time: new Date(2015, 10, 30, 18, 0),
+					reminderTime: new Date(2015, 10, 30, 17, 0),
+					description: "Remember to bering beer",
+					isReminderSent: false
 				},
 				{
 					title: "meeting with boss",
-					time: new Date(2015, 10, 25),
-					description: ""
+					time: new Date(2015, 10, 25, 9, 0),
+					reminderTime: new Date(2015, 10, 25, 8, 0),
+					description: "",
+					isReminderSent: false
 				},
 				{
 					title: "doctor's appointment",
-					time: new Date(2015, 10, 26)
+					time: new Date(2015, 10, 26, 15, 0),
+					reminderTime: new Date(2015, 10, 26, 10, 0),
+					isReminderSent: false
 				}
 			];
 			
@@ -117,27 +122,41 @@
 	function checkReminders() {
 		eventsViewModel.data.forEach(function(event) {
 			var now = new Date();
-			if (event.time > now) {
-				if (event.reminderTime <= now) {
+			if( ! event.isReminderSent) {
+				if (event.time > now && event.reminderTime <= now) {
 					var msg = event.title + " at " + event.formattedTime()
 					console.log(msg);
+					alert(msg);
 					if (document['hidden']) {
 						new Notification(msg);
 					}
-				}
+					event.isReminderSent = true;
+					events.update(event);
+					renderList();
+				}				
 			}
+
 		}, this);
 	}
 	
 	function renderList() {	
 		function onFinishedFetchingData(data) {
-			var templateData = { eventsList: data };
+			var sortedData = data.sort(function sortByTime(a, b) {
+				if(a.time < b.time)
+					return -1;
+				else if (a.time > b.time)
+					return 1;
+					
+				return 0
+			});
+			var templateData = { eventsList: sortedData };
 			var output = template(templateData);
 			//x = data;
-			$eventsList.html(output);
+			$eventsList.html(output); 
 			registerHandlers();
 			window.setInterval(checkReminders, 10*1000);	
 		}
+		
 		events.getAll(onFinishedFetchingData);	
 	}
 	
@@ -165,42 +184,74 @@
 	
 	function getTimeFromInput($element) {
 		var dateTimeLocalStr = $element.val();
+		if (dateTimeLocalStr === ""){
+			return dateTimeLocalStr;
+		}
 		var dateTime = new Date(dateTimeLocalStr);
-		dateTime.setHours(dateTime.getHours() - 2);
+		dateTime.setHours(dateTime.getHours() + dateTime.getTimezoneOffset()/60);
 		return dateTime;
 	}	
 
 	$(".add-event-btn").on("click", function addEvent(ev) {
 		ev.preventDefault();
-		var isModelValid = $newEventTitle.val() && $newEventTime.val() 
-		if (isModelValid) {
-			var eventTime = new Date($newEventTime.val());
-			eventTime.setHours(eventTime.getHours() - 1);
+		var eventTime = getTimeFromInput($newEventTime);
+		var reminderTime = getTimeFromInput($newEventReminderTime);
+		
+		var validateForm = function() {
+			var result = {
+				isValid: false,
+				errors: [],
+			};
+			var areRequiredFieldsFilled = $newEventTitle.val() && $newEventTime.val() 
+			var isReminderTimeLessThanEventTime = reminderTime < eventTime;
 			
-			var params = {
-				id: Number($(".new-event-id").val()),
+			if(! areRequiredFieldsFilled)
+				result.errors.push("Event title and time are required.")
+			if (! isReminderTimeLessThanEventTime)
+				result.errors.push("Reminder time must be before the event.")
+			if (areRequiredFieldsFilled && isReminderTimeLessThanEventTime) {
+				result.isValid = true;
+			}	
+			
+			return result;	
+		}
+		
+		var validationResult = validateForm();
+		
+		if (validationResult.isValid) {			
+			var event = {
 				title: $newEventTitle.val(),
 				description: $newEventDescription.val(),
 				time: getTimeFromInput($newEventTime),
 				reminderTime: getTimeFromInput($newEventReminderTime)
 			};
+			if ($(".new-event-id").val()) {
+				event.id = Number($(".new-event-id").val());		
+			}
 			
-			if (! params.id) {
-				events.add(new MyEvent(params));	
+			if (! event.id) {
+				events.add(event);	
 			} else {
-				events.update(new MyEvent(params));
+				events.update(event);
 			}
 				
 			$(".new-event-form")[0].reset();
+			$(".new-event-id").val("");
 			$newEventTime.val(new Date().toDateInputValue());
 			$newEventReminderTime.val(new Date().toDateInputValue());
 			renderList();
 		} else {
-			alert("Please specify the name and time of event.")
+			var msg = "";
+			validationResult.errors.forEach(function(error) {
+				msg += error + '\n';
+			}, this);
+			alert(msg)
 		}
 	});
 	
-
+	$(".clear-form").on("click", function clearForm() {
+		$(".new-event-form")[0].reset();
+	});
 	
 	var $eventsList = $(".events-list");
 	var templateSource = $("#event-template").html();
